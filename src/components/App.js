f0769b07f340d88695abfeba49a3a2efd7b3e981
Israt-Jahan-Mouri <mouri2018@gmail.com>
2020-12-15 20:31:56 +0600

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Dashboard from '../pages/Dashboard';
